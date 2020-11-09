69bfc39bb0cb666a4c0f748f927911ebc43d9840
Moataz Hammous <moataz.hammous@pixelogicmedia.com>
2020-11-09 23:49:49 +0200

    Update running newman

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const newman = require('newman');
 const start = () => {
 	try {
 		const newmanOptions = core.getInput('options');
 		console.log(`Newman Options:${newmanOptions}`);
-		// Get the JSON webhook payload for the event that triggered the workflow
-		const payload = JSON.stringify(github.context.payload, undefined, 2)
-		console.log(`The event payload: ${payload}`);
 		//Start executing postman collection
 		executeNewman(newmanOptions);
 	} catch (error) {
@@ -15,12 +11,12 @@ const start = () => {
 	}
 }
 const executeNewman = (newmanOptions) => {
-	newman.run(newmanOptions).on('done', (err, summary) => {
-		if (err || summary.run.failures.length > 0) {
-			core.setFailed(`Newman run failed! ${err}`);
-		} else {
-			core.debug('Collection run completed.')
+	console.log("Starting Running newman")
+	newman.run(newmanOptions, function (err) {
+		if (err) {
+			throw err;
 		}
+		console.log('collection run complete!');
 	})
 }
 
